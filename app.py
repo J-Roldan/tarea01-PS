17a0782650eede4d4faccbb53ce05f54da49c46d
J-Roldan <102828233+J-Roldan@users.noreply.github.com>
2024-04-12 16:40:08 -0400

    User login partially implemented

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 from flask import Flask, request, jsonify
 from password_generator import random_password
-from password_encrypt import generateKey, encryptPassword, decryptPassword
+from password_encrypt import generateKey, encryptPassword, decryptPassword, encryptMasterPassword, verifyMasterPassword
 import logging
 
 # Configure logging
 logging.basicConfig(filename='app.log', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 app = Flask(__name__)
 
+
+# users = {username: {hashedPassword, key, passwords={}}}
+users = {}
 passwords = {}
 
 """ 
@@ -23,6 +26,48 @@ def index():
 def server_error(error):
     logging.exception('An exception occurred during a request.')
     return 'Internal Server Error', 500
+
+# Create a new user
+@app.route('/users', methods=['POST'])
+def add_user():
+    data = request.get_json()
+    username = data.get('username')
+    password = data.get('password')
+
+    if not (username and password):
+        message = f'Username and password fields are required'
+        logging.error(message)
+        return jsonify({'error': message}), 400
+    
+    if username in users:
+        message = f"Username '{username}' already exists."
+        logging.error(message)
+        return jsonify({'error': message}), 400
+    
+    key = generateKey()
+    hashedPassword = encryptMasterPassword(password)
+    users[username] = {'hashedPassword': hashedPassword, 'key': key, 'passwords': {}}
+    message = f"User '{username}' signed succesfully."
+    logging.info(message)
+    return jsonify({'message': message, 'username': username}), 200
+
+@app.route('users/<username>', methods=['GET'])
+def get_passwords(username):
+    password = request.args.get('password')
+    if not username in users:
+        message = f'Username doesn\'t exist'
+        logging.error(message)
+        return jsonify({'error': message}), 400
+    if not (verifyMasterPassword(password, users[username]['hasedPassword'])):
+        message = f'Password incorrect'
+        logging.error(message)
+        return jsonify({'error': message}), 400
+
+    global passwords
+    passwords = users[username]['passwords']
+    message = f"User '{username} signed succesfully'."
+    logging.info(message)
+    return jsonify({'message': message, 'username': username}), 200
 
 # Create (Add) a new password with a keyword
 @app.route('/passwords', methods=['POST'])
